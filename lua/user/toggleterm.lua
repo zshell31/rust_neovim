0b7ecb5f2c2a4f1c9d65fd2d1b40bb048e77a88a
Evgeny Nosov <evgen3188@gmail.com>
2022-04-30 17:16:11 +0300

    fix toggleterm keymaps & some terminal commands

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -24,12 +24,12 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-q>', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', 'i', ":starti", opts)
+  vim.api.nvim_buf_set_keymap(0, 'n', 'i', ":starti<CR>", opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
@@ -37,16 +37,20 @@ vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 local Terminal = require("toggleterm.terminal").Terminal
 
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-function _LAZYGIT_TOGGLE()
+function _G.lazygit_toggle()
 	lazygit:toggle()
 end
+vim.cmd [[command! LazyGit lua lazygit_toggle()]]
+
+local gitlog_cmd = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'"
+local gitlog = Terminal:new({ cmd = gitlog_cmd, hidden = true })
+function _G.gitlog_toggle()
+	gitlog:toggle()
+end
+vim.cmd [[command! GitLog lua gitlog_toggle()]]
 
 local htop = Terminal:new({ cmd = "htop", hidden = true })
-function _HTOP_TOGGLE()
+function _G.htop_toggle()
 	htop:toggle()
 end
-
-local python = Terminal:new({ cmd = "ipython", hidden = true })
-function _PYTHON_TOGGLE()
-	python:toggle()
-end
+vim.cmd [[command! HTop lua htop_toggle()]]
