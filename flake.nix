3929399d8f5f2b798f4cce0a5f4eb89ff7b34238
zshell31 <evgen3188@gmail.com>
2022-05-04 22:39:01 +0300

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+
+  outputs = { self, nixpkgs }:
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell.${system} =
+        pkgs.mkShell { nativeBuildInputs = with pkgs; [ 
+          pkg-config
+          cmake
+        ]; 
+      };
+   };
+}
+
